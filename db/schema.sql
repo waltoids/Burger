141649984ddb9ca5d71b8c9feed712fa8e1e53de
Walter Joy IV <waltoids@users.noreply.github.com>
2020-07-20 18:37:47 -0400

    fixed table burgers field

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE burgers_db;
 
 CREATE TABLE burgers (
     id INT AUTO_INCREMENT NOT NULL,
-    buger_name VARCHAR(255),
+    burger_name VARCHAR(255),
     devoured BOOLEAN DEFAULT FALSE,
     PRIMARY KEY (id)
 );
