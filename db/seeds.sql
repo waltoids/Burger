dd52ef1c91ac4464ab2fee99828fa0fc883b4d71
Walter Joy IV <waltoids@users.noreply.github.com>
2020-07-20 18:35:05 -0400

    added seeds for burger db

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Bacon Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Curd Burgers', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baisc Burgers', FALSE);
