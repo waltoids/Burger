35835c93018067ada45cc5d7ab6f33e499bb5dfe
Walter Joy IV <waltoids@users.noreply.github.com>
2020-07-21 21:54:17 -0400

    fixed an issue with objects not turning into strings to query, added a helper method

--- config/orm.js
+++ config/orm.js
@@ -1,6 +1,26 @@
 // Import MySQL connection.
 const connection = require('./connection');
 
+function objToSql(ob) {
+  var arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
+
 // Object for all our SQL statement functions.
 const orm = {
     selectAll: function(tableName, cb) {
@@ -13,25 +33,29 @@ const orm = {
       });
     },
     insertOne: function(tableName, columns, values, cb) {
-        const queryString = "INSERT INTO " + tableName + " (" + columns.toSting() + ") VALUES (?) ";
+        const queryString = "INSERT INTO " + tableName + " (" + columns.toString() + ") VALUES (?) ";
     
         connection.query(queryString, values, function(err, result) {
           if (err) {
             throw err;
           }
-    
           cb(result);
         });
       },
-     updateOne: function(tableName, column, colValue, field, fieldValue, cb) {
-        const queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
+     updateOne: function(tableName, objColVals, condition, cb) {
+        let queryString = "UPDATE " + tableName;
+
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+
     
         console.log(queryString);
-        connection.query(queryString, [tableName, column, colValue, field, fieldValue], function(err, result) {
+        connection.query(queryString, function(err, result) {
           if (err) {
             throw err;
           }
-    
           cb(result);
         });
       }
