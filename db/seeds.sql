a4837f17dc9a8f7e7e4c910506590b1de7680009
Walter Joy IV <waltoids@users.noreply.github.com>
2020-07-23 21:39:19 -0400

    edited seeds to not use a default database

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,3 @@
-USE burgers_db;
-
 INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Bacon Burger', FALSE);
 INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Curd Burgers', FALSE);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Baisc Burgers', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Basic Burgers', FALSE);
